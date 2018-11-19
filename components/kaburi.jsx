9bea9c2e25fce7de1d1608f17887d37a3f78763a
kohei <watanabe_kohei@waku-2.com>
2018-11-19 17:26:42 +0900

    refactor: Stateless Functional Componentに変更

--- components/kaburi.jsx
+++ components/kaburi.jsx
@@ -1,20 +1,16 @@
-import { Component } from "react";
+export default ({ activity }) => {
+  const kaburiActivity = activity.filter(a => a.ch === "kaburi");
+  const count = kaburiActivity.length;
+  const time = kaburiActivity
+    .map(a => a.endTime - a.startTime)
+    .reduce((a, c) => a + c, 0);
 
-export default class extends Component {
-  render() {
-    const activity = this.props.activity.filter(a => a.ch === "kaburi");
-    const count = activity.length;
-    const time = activity
-      .map(a => a.endTime - a.startTime)
-      .reduce((a, c) => a + c, 0);
-
-    return (
-      <dl>
-        <dt>かぶり回数</dt>
-        <dd>{count}</dd>
-        <dt>かぶり時間 (ms)</dt>
-        <dd>{time}</dd>
-      </dl>
-    );
-  }
-}
+  return (
+    <dl>
+      <dt>かぶり回数</dt>
+      <dd>{count}</dd>
+      <dt>かぶり時間 (ms)</dt>
+      <dd>{time}</dd>
+    </dl>
+  );
+};
--- components/percentage.jsx
+++ components/percentage.jsx
@@ -1,29 +1,25 @@
-import { Component } from "react";
+export default ({ activity }) => {
+  const leftActivity = activity
+    .filter(a => a.ch === "l")
+    .map(a => a.endTime - a.startTime)
+    .reduce((a, c) => a + c, 0);
+  const rightActivity = activity
+    .filter(a => a.ch === "r")
+    .map(a => a.endTime - a.startTime)
+    .reduce((a, c) => a + c, 0);
 
-export default class extends Component {
-  render() {
-    const leftActivity = this.props.activity
-      .filter(a => a.ch === "l")
-      .map(a => a.endTime - a.startTime)
-      .reduce((a, c) => a + c, 0);
-    const rightActivity = this.props.activity
-      .filter(a => a.ch === "r")
-      .map(a => a.endTime - a.startTime)
-      .reduce((a, c) => a + c, 0);
-
-    return (
-      <dl>
-        <dt>会話時間 (ms)</dt>
-        <dd>{leftActivity + rightActivity}</dd>
-        <dt>会話の割合</dt>
-        <dd>
-          左{" "}
-          <meter value={leftActivity / (leftActivity + rightActivity)}>
-            {leftActivity / (leftActivity + rightActivity)}
-          </meter>{" "}
-          右
-        </dd>
-      </dl>
-    );
-  }
-}
+  return (
+    <dl>
+      <dt>会話時間 (ms)</dt>
+      <dd>{leftActivity + rightActivity}</dd>
+      <dt>会話の割合</dt>
+      <dd>
+        左{" "}
+        <meter value={leftActivity / (leftActivity + rightActivity)}>
+          {leftActivity / (leftActivity + rightActivity)}
+        </meter>{" "}
+        右
+      </dd>
+    </dl>
+  );
+};
