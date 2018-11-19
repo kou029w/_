0508d129c7a528514ad505164c38f8dfeea99001
kohei <watanabe_kohei@waku-2.com>
2018-11-19 18:46:47 +0900

    refactor: 実行プロセスを修正

--- components/percentage.jsx
+++ components/percentage.jsx
@@ -1,24 +1,20 @@
 export default ({ activity }) => {
-  const leftActivity = activity
-    .filter(a => a.ch === "l")
-    .map(a => a.endTime - a.startTime)
-    .reduce((a, c) => a + c, 0);
-  const rightActivity = activity
-    .filter(a => a.ch === "r")
-    .map(a => a.endTime - a.startTime)
-    .reduce((a, c) => a + c, 0);
+  const leftActivity = activity.filter(a => a.ch === "l"),
+    rightActivity = activity.filter(a => a.ch === "r");
+  const [l, r] = [leftActivity, rightActivity].map(activity => {
+    return activity
+      .map(a => a.endTime - a.startTime)
+      .reduce((a, c) => a + c, 0);
+  });
+  const sum = l + r;
 
   return (
     <dl>
       <dt>会話時間 (ms)</dt>
-      <dd>{leftActivity + rightActivity}</dd>
+      <dd>{sum}</dd>
       <dt>会話の割合</dt>
       <dd>
-        左{" "}
-        <meter value={leftActivity / (leftActivity + rightActivity)}>
-          {leftActivity / (leftActivity + rightActivity)}
-        </meter>{" "}
-        右
+        左 <meter value={l / sum}>{l / sum}</meter> 右
       </dd>
     </dl>
   );
