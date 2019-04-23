4cfc54cfd0335302656c58a096061e095c470e52
Kohei Watanabe <kou029w@gmail.com>
2019-04-23 17:34:03 +0900

    MDX使う

--- next.config.js
+++ next.config.js
@@ -1,2 +1,14 @@
 const withTypescript = require("@zeit/next-typescript");
-module.exports = withTypescript();
+const withMDX = require("@zeit/next-mdx");
+
+module.exports = [
+  [
+    withMDX({
+      extension: /\.mdx?$/
+    }),
+    {
+      pageExtensions: ["md", "mdx"]
+    }
+  ],
+  [withTypescript]
+].reduce((o, [f, a]) => f(Object.assign(o, a)), {});
