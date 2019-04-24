c8c3ec30dcfdf983bbed7149e54a76f4c1759407
Kohei Watanabe <kou029w@gmail.com>
2019-04-24 17:21:31 +0900

    余計なので削除

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,7 @@ export default class extends Document {
   render() {
     return (
       <html lang="ja" dir="ltr">
-        <Head>
-          <meta charSet="utf-8" />
-        </Head>
-
+        <Head />
         <body>
           <Main />
           <NextScript />
