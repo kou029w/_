9414419f73e2d822819454ad0333d7b8be8afbd7
Kohei Watanabe <kou029w@gmail.com>
2020-02-08 22:08:02 +0900

    use NEXT_ASSET_PREFIX

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = require("@next/mdx")({
   extension: /\.mdx?$/
 })({
-  pageExtensions: ["ts", "tsx", "md", "mdx"]
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
+  assetPrefix: process.env.NEXT_ASSET_PREFIX || ""
 });
