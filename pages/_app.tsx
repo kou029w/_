a27ea730b93c372261a5e627c0db618d4ed7092f
Kohei Watanabe <kou029w@gmail.com>
2020-02-09 03:17:15 +0900

    duplicatedなので

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,14 @@
 import React from "react";
-import App, { Container } from "next/app";
+import App from "next/app";
 import MainTheme from "../styles/MainTheme";
 
 export default class extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <Container>
-        <MainTheme>
-          <Component {...pageProps} />
-        </MainTheme>
-      </Container>
+      <MainTheme>
+        <Component {...pageProps} />
+      </MainTheme>
     );
   }
 }
