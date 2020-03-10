545c59e65d9e5d44b090bcfcde4d804a277c221f
Kohei Watanabe <kou029w@gmail.com>
2020-03-10 17:26:17 +0900

    use getStaticPaths/getStaticProps

---
+++ pages/query/[path].tsx
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+
+export function getStaticPaths() {
+  return { paths: [{ params: { path: "test" } }], fallback: true };
+}
+
+export function getStaticProps({ params }: { params: unknown }) {
+  return { props: params };
+}
+
+export default function({ path }: { path: string }) {
+  const { query } = useRouter();
+  return <pre>{JSON.stringify({ path, ...query }, null, "  ")}</pre>;
+}
