19b6c91e86e4f86870096802cb8f3122531013b0
Kohei Watanabe <kou029w@gmail.com>
2020-07-02 10:21:06 +0900

    fix: basePath support

--- next.config.js
+++ next.config.js
@@ -2,7 +2,5 @@ module.exports = require("@next/mdx")({
   extension: /\.mdx?$/,
 })({
   pageExtensions: ["ts", "tsx", "md", "mdx"],
-  experimental: {
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
-  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
 });
