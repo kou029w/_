679f725e02399a1afe968464c61709d3c546cb01
Kohei Watanabe <kou029w@gmail.com>
2020-07-27 20:06:30 +0900

    Unhandled promise rejection terminates Node.js process with non-zero exit code.

--- playwright/main.js
+++ playwright/main.js
@@ -1,6 +1,11 @@
 const playwright = require("playwright");
 
 async function main() {
+  // NOTE: Unhandled promise rejection terminates Node.js process with non-zero exit code.
+  process.on("unhandledRejection", (event) => {
+    throw event;
+  });
+
   for (const browserType of ["chromium", "firefox", "webkit"]) {
     const browser = await playwright[browserType].launch();
     const context = await browser.newContext();
