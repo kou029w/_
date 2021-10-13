db8b7490c77f5abfad75f834a07e62ac6fe819d3
Kohei Watanabe <kou029w@gmail.com>
2021-10-13 19:50:32 +0900

    refactor

--- vite-ssg/renderer/_default.page.server.tsx
+++ vite-ssg/renderer/_default.page.server.tsx
@@ -3,14 +3,16 @@ import { escapeInject, dangerouslySkipEscape } from "vite-plugin-ssr";
 import type { PageContextBuiltIn } from "vite-plugin-ssr/types";
 
 export async function render(pageContext: PageContextBuiltIn) {
-  const { Page } = pageContext;
-  const html = renderToString(<Page />);
-
-  return escapeInject`<!DOCTYPE html>
+  const html = renderToString(
     <html>
       <head>
         <title>vite-ssg-example</title>
       </head>
-      <body>${dangerouslySkipEscape(html)}</body>
-    </html>`;
+      <body>
+        <pageContext.Page />
+      </body>
+    </html>
+  );
+
+  return escapeInject`<!DOCTYPE html>${dangerouslySkipEscape(html)}`;
 }
