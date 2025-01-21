8f13edab1ed100356af83c05250fdcf6917df2e5
Kohei Watanabe <nebel@fogtype.com>
2025-01-21 11:25:36 +0900

    remove fastify-validation package.json and rename server.mjs to server.ts with updated routes

--- fastify-validation/server.ts
+++ fastify-validation/server.ts
@@ -1,8 +1,58 @@
-import fastify from "fastify";
-import { parse as JsonParse } from "secure-json-parse";
+#!/usr/bin/env -S deno run -A
+import fastify from "npm:fastify";
+// import { parse as jsonParse } from "npm:secure-json-parse";
 
 const server = fastify({ logger: true });
 
+server.get("/anyOf", {
+  schema: {
+    querystring: {
+      type: "object",
+      properties: {
+        array: {
+          anyOf: [
+            { type: "array", items: { type: "string" } },
+            { type: "string" },
+          ],
+        },
+      },
+      required: ["array"],
+    },
+  },
+  handler({ query }) {
+    console.log({ ...query });
+    return "ok";
+  },
+});
+
+server.get("/oneOf", {
+  schema: {
+    querystring: {
+      type: "object",
+      properties: {
+        array: {
+          oneOf: [
+            { type: "array", items: { type: "string" } },
+            { type: "string" },
+          ],
+        },
+      },
+      required: ["array"],
+    },
+  },
+  handler({ query }) {
+    console.log({ ...query });
+    return "ok";
+  },
+});
+
+// valid
+await server.inject(`/anyOf?array=a`);
+
+// invalid: querystring/array must match exactly one schema in oneOf
+await server.inject(`/oneOf?array=a`);
+
+/*
 server.get("/array", {
   schema: {
     querystring: {
@@ -14,7 +64,7 @@ server.get("/array", {
   async preValidation(req) {
     req.query.array = [req.query.array].flat().flatMap((s) => {
       try {
-        const parsed = JsonParse(s);
+        const parsed = jsonParse(s);
         return Array.isArray(parsed) ? parsed : [s];
       } catch {
         return [s];
@@ -29,6 +79,7 @@ server.get("/array", {
 
 // valid
 await server.inject(`/array?array=["123",4]`);
+*/
 
 /*
 server.get("/dateTime", {
