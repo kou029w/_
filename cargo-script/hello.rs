ba75691d5fea81e684a04d7642a4e3accb443a16
Kohei Watanabe <nebel@fogtype.com>
2025-11-30 15:51:28 +0900

    use clap

--- cargo-script/hello.rs
+++ cargo-script/hello.rs
@@ -1,5 +1,23 @@
 #!/usr/bin/env -S cargo -Zscript -q
+---
+[dependencies]
+clap = { version = "4.5.53", features = ["derive"] }
+---
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long)]
+    name: String,
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
 
 fn main() {
-    println!("hello");
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name);
+    }
 }
